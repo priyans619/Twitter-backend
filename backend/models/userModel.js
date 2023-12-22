80fc702e232ed5d7f28bfda2974c7e94a7a2b3d0
priyans619 <priyansh.2b@gmail.com>
2023-12-22 21:35:37 +0530

    add userModel with 'username' and 'password'

---
+++ backend/models/userModel.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    password:
+    {
+      type: String,
+      required: true
+    },
+  });
+
+  
+export const User = mongoose.model('User', userSchema);
