5f0dc40b1ae14c361794d3587e9136043918be51
priyans619 <priyansh.2b@gmail.com>
2023-12-22 17:11:55 +0530

    App connected to mongo Database using mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 
-import { PORT } from './config.js';
+import { PORT, mongoURL } from './config.js';
+import mongoose from 'mongoose';
 
 const app = express();
 
@@ -9,3 +10,14 @@ app.get('/');
 app.listen(PORT, () => {
   console.log('App is listening to port: ${PORT}')
 })
+
+
+mongoose
+.connect(mongoURL)
+.then(() => {
+    console.log('Server is connected to database');
+})
+.catch((error) => {
+    console.log(error);
+
+});
